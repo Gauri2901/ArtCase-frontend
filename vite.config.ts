b54218cbf2af8b91387fcb86471207b01ed8ded4
Jeevan <omborle14@gmail.com>
2026-04-15 15:57:27 +0530

    Solved: Some chunks are larger than 500 kB

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'vendor-motion';
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            if (id.includes('react')) return 'vendor-react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
